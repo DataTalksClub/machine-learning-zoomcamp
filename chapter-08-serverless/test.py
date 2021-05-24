88fa4273677f35d9cc313d2269e25a5d9cf88212
Alexey Grigorev <alexey.grigorev@olx.com>
2021-05-24 14:56:38 +0200

    making sure it runs with python 3.8 and latest anaconda + some fixes

--- chapter-08-serverless/test.py
+++ chapter-08-serverless/test.py
@@ -4,8 +4,8 @@ data = {
     "url": "http://bit.ly/mlbookcamp-pants"
 }
 
-# url = "http://localhost:8080/2015-03-31/functions/function/invocations"
-url = "https://0a1v3fyo2m.execute-api.eu-west-1.amazonaws.com/test/predict"
+url = "http://localhost:8080/2015-03-31/functions/function/invocations"
+# url = "https://0a1v3fyo2m.execute-api.eu-west-1.amazonaws.com/test/predict"
 
 results = requests.post(url, json=data).json()
 
