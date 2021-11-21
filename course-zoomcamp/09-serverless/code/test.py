794731fe9fc548a798c5a0ffeefa9bc21bc77e15
Alexey Grigorev <alexeygrigorev@users.noreply.github.com>
2021-11-21 23:29:36 +0100

    Update test.py

--- course-zoomcamp/09-serverless/code/test.py
+++ course-zoomcamp/09-serverless/code/test.py
@@ -1,7 +1,6 @@
 import requests
 
-#url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-url = 'https://rpa3mf7j86.execute-api.eu-west-1.amazonaws.com/test/predict'
+url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 
 data = {'url': 'http://bit.ly/mlbookcamp-pants'}
 
